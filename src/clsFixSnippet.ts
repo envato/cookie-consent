94bffd564ca0f0b372f1475327f099227e02dca7
Pawel Galazka <829242+pawelgalazka@users.noreply.github.com>
2021-05-10 16:15:31 +1000

    Provide cls fix snippet as string

---
+++ src/clsFixSnippet.ts
@@ -0,0 +1,17 @@
+export default `
+  window.addEventListener("CookiebotOnDialogDisplay", (event) => {
+    const cookieDialog = event.srcElement.CookieConsentDialog.DOM
+    cookieDialog.dataset.turbo = false
+
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach(() => {
+        if (!cookieDialog.style.transform.match(cookieDialog.style.top)) {
+          cookieDialog.style.setProperty("transform", \`translateY(\${cookieDialog.style.top})\`)
+          cookieDialog.style.setProperty("opacity", "1", "important")
+        }
+      })
+    })
+
+    observer.observe(cookieDialog, { attributes: true, attributeFilter: ["style"] })
+  })
+`
